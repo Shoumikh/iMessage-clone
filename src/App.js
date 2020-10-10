93ebef8279dd1721ddd837dcab5973d638ce9b4f
Shahnewaj Maksud Shoumikh <ssoumikh@gmail.com>
2020-10-10 10:01:33 +0600

    authentication done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { selectUser } from "./features/userSlice";
+import { selectUser, login, logout } from "./features/userSlice";
 import Imessage from "./Imessage";
 import Login from "./Login";
 import { auth } from "./firebase";
@@ -11,14 +11,22 @@ function App() {
   const dispatch = useDispatch(); //for dispathing the value in the data layer
 
   useEffect(() => {
-    auth.onAuthStateChanged(authUser => {
-       if(authUser){
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
         //user is logged in
-      }else{
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
         //user is logged out
+        dispatch(logout());
       }
-    }
-     )
+    });
   }, []);
   return <div className="App">{user ? <Imessage /> : <Login />}</div>;
 }
